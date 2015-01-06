71e7ada82a2efe846400fb64fe317c9540f4b725
nymph332088 <lvzongping@gmail.com>
2015-01-06 16:37:11 -0500

    website

---
+++ markdown/Lab Assignment 1.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+
+# <markdowncell>
+
+# #Here's an example:
+# ##another example
+# 
+# ```
+# function test() {
+#   console.log("notice the blank line before this function?");
+# }
+# ```
+# 
+# - [ ] a bigger project
+#   - [ ] first subtask #1234
+#   - [ ] follow up subtask #4321
+#   - [ ] final subtask cc @mention
+# - [ ] a separate task
+
+# <codecell>
+
+function test() {
+  console.log("notice the blank line before this function?");
+}
+
