e676b24d90e0285d61dd3977713de78084aa3d2c
nymph332088 <lvzongping@gmail.com>
2015-01-21 01:43:04 -0500

    4340 Lab 2 modification

--- CIS4340/labassignments/Lab2/wordcount.py
+++ CIS4340/labassignments/Lab2/wordcount.py
@@ -6,17 +6,51 @@ Created on Sun Jan 11 18:39:41 2015
 """
 import sys
 
-def top_k(k, filename):
-    topK = []
+def print_dict(d):
+	pass
+	
+def top_k(k, wordcount):
+    topK = {}
     return topK
 
 def word_count(filename):
-    wordcount = []
+    wordcount = {}
     print 'Hello ' + filename
     with open(filename) as f:
          print 'Number of lines: ' + str(len(f.readlines()))
     return wordcount
 
+def word_count_2():
+	wordcount = {}
+	return wordcount
+
+def clean():
+	pass
+
 if __name__ == '__main__':
+
+	# Part 2
     wordCount = word_count(sys.argv[1])
-    topK = top_k(10, sys.argv[1])
+    print_dict(wordCount)
+    
+    topK = top_k(10, wordCount)
+    print_dict(wordCount)
+    
+    # Part 3
+    wordCount2 = word_count_2()
+    print_dict(wordCount2)
+    
+    topK = top_k(10, wordCount)
+    print_dict(wordCount2)
+    
+    
+    # Part 4
+#    clean()
+#    wordCount_Cleaned = word_count(sys.argv[1])
+#    print_dict(wordCount_Cleaned)
+#    
+#    topK_Cleaned = top_k(10, wordCount_Cleaned)
+#    print_dict(topK_Cleaned)
+    
+    
+    
