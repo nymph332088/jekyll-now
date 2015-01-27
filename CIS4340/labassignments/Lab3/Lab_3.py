e694a0e2c605aa0254f423694d617c3a56480d30
nymph332088 <lvzongping@gmail.com>
2015-01-27 03:00:41 -0500

    Lab 3 4340

---
+++ CIS4340/labassignments/Lab3/Lab_3.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 27 02:33:19 2015
+
+@author: nymph
+"""
+
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+''' Write your code for loading and preprocess the data'''
+
+def plot1():
+    pass
+
+def plot2():
+    pass
+
+def plot3():
+    pass
+
+def plot4():
+    pass
+
